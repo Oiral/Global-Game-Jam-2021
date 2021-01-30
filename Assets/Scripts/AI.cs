e678db49238fc16b91e8a1980d27213d8a6a6772
Hamish <hamish@pageland.co.nz>
2021-01-31 11:39:35 +1300

    Much Better AI

--- Assets/Scripts/AI.cs
+++ Assets/Scripts/AI.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-public enum AIState { tracking, wander, foundPlayer}
+public enum AIState { tracking, wander, foundPlayer, surveying}
 
 [RequireComponent(typeof(NavMeshAgent))]
 public class AI : MonoBehaviour
@@ -11,12 +11,18 @@ public class AI : MonoBehaviour
     public AIState currentState = AIState.foundPlayer;
 
     NavMeshAgent agent;
+    float startingAgentSpeed;
+    float startingAgentAcceleration;
 
     public GameObject player;
 
     public float viewRange;
 
     public Animator animator;
+
+    bool trackingPlayer;
+
+    public LayerMask rayMask;
 
     private void OnDrawGizmos()
     {
@@ -46,6 +52,9 @@ public class AI : MonoBehaviour
     {
         agent = GetComponent<NavMeshAgent>();
 
+        startingAgentSpeed = agent.speed;
+        startingAgentAcceleration = agent.acceleration;
+
         wanderPosition = transform.position;
 
         animator.SetBool("Moving", true);
@@ -56,34 +65,152 @@ public class AI : MonoBehaviour
 
     Vector3 wanderPosition = Vector3.zero;
 
-    
+    float surveyTimer;
+    float surveyCoolDown;
+
+    float chargeTimer;
+    Vector3 lastSeenPlayerPos;
 
     // Update is called once per frame
     void Update()
     {
+        if (surveyCoolDown >= 0)
+        {
+            surveyCoolDown -= Time.deltaTime;
+        }
 
-        if (CanSeePlayer())
+        /*
+        if (CanSeePlayer() || trackingPlayer)
         {
             currentState = AIState.foundPlayer;
         }
         else
         {
-            currentState = AIState.wander;
-        }
+            if (Vector3.Distance(transform.position, player.transform.position) > 20 || surveyCoolDown >= 0)
+            {
+                currentState = AIState.wander;
+            }
+            else
+            {
+                currentState = AIState.surveying;
+            }
+            
+        }*/
 
         switch (currentState)
         {
             case AIState.wander:
+
+                
                 if (Vector3.Distance(agent.destination, transform.position) < gotToDestinationSize)
                 {
-                    Debug.Log("Seaching for point");
-                    Vector3 point;
-                    if (RandomPointOnNavMesh(wanderPosition, wanderRange, out point))
+                    if (trackingPlayer)
                     {
-                        //Debug.DrawRay(point, Vector3.up, Color.blue, 1.0f);
-                        agent.SetDestination(point);
+                        animator.SetBool("Player In View", false);
+                        trackingPlayer = false;
+                        agent.speed = startingAgentSpeed;
+                        agent.acceleration = startingAgentAcceleration;
+                        currentState = AIState.surveying;
+                    }
+                    else
+                    {
+                        Debug.Log("Seaching for point");
+                        Vector3 point;
+                        if (RandomPointOnNavMesh(wanderPosition, wanderRange, out point))
+                        {
+                            //Debug.DrawRay(point, Vector3.up, Color.blue, 1.0f);
+                            animator.SetBool("Moving", true);
+                            agent.SetDestination(point);
+                        }
                     }
                 }
+
+                if (Vector3.Distance(transform.position, player.transform.position) < 20 && surveyCoolDown <= 0 && trackingPlayer == false)
+                {
+                    currentState = AIState.surveying;
+                    break;
+                }
+                break;
+
+            case AIState.surveying:
+                surveyTimer += Time.deltaTime;
+
+                agent.destination = transform.position;
+                animator.SetBool("Moving", false);
+
+                if (CanSeePlayer())
+                {
+                    surveyTimer = 0;
+                    currentState = AIState.foundPlayer;
+                    break;
+                }
+
+                if (surveyTimer > 5)
+                {
+                    surveyCoolDown = Random.Range(3f,5f);
+                    surveyTimer = 0;
+                    currentState = AIState.wander;
+                }
+                break;
+
+            case AIState.foundPlayer:
+                //we want to let the minotaur roar
+                animator.SetBool("Moving", false);
+                animator.SetBool("Player In View", true);
+
+                agent.SetDestination(transform.position + (player.transform.position - transform.position).normalized * 0.1f);
+
+                if (trackingPlayer == false)
+                {
+                    if (CanSeePlayer())
+                    {
+                        lastSeenPlayerPos = player.transform.position;
+                    }
+
+                    
+
+                    chargeTimer += Time.deltaTime;
+                    if (chargeTimer < 2)
+                    {
+                        //If we are not ready to charge
+                        
+                        break;
+                    }
+                }
+
+                //We can only get here when it is time to charge
+
+                chargeTimer = 0;
+                //We want to set the destination
+                //We want to charge past the last seen player pos
+                RaycastHit hit;
+
+                Vector3 dir = lastSeenPlayerPos - transform.position;
+                dir.y = 0;
+
+                Debug.DrawRay(transform.position, (dir) * 40f, Color.red,1f);
+
+                if (Physics.Raycast(transform.position, dir, out hit, 20f,rayMask))
+                {
+                    Debug.DrawRay(transform.position, hit.point - transform.position, Color.red, 1f);
+
+                    agent.SetDestination(hit.point);
+                }
+                else
+                {
+                    //If we don't hit any walls, Dont go more than 20 units away
+                    agent.SetDestination(transform.position + dir.normalized * 20f);
+                    trackingPlayer = true;
+                }
+
+                surveyCoolDown = 2;
+                animator.SetBool("Moving", true);
+                trackingPlayer = true;
+
+                agent.speed = startingAgentSpeed * 1.2f;
+                agent.acceleration = startingAgentAcceleration * 1.2f;
+
+                currentState = AIState.wander;
                 break;
 
             default:
@@ -104,11 +231,9 @@ public class AI : MonoBehaviour
             //If what we hit was the player
             if (hit.transform.tag == "Player")
             {
-                animator.SetBool("Player In View", true);
                 return true;
             }
         }
-        animator.SetBool("Player In View", false);
         return false;
     }
 
